52e052c7880f48e072290ca170800fb4481aa411
feyrose2 <123549917+feyrose2@users.noreply.github.com>
2023-01-25 11:21:44 -0800

    Add files via upload

---
+++ scripts/myScript.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+        $('h1').css('color','#069'); 
+        alert('Hi there!');
+    });
+
+// comment out a line 
+
+
+
+/* shorthand- 
+
+$(function(){
+
+})
+*/
+/*
+$(document).ready(function(){
+    $('button').click(function(){
+        $('p').text('Here is some brand new text!');
+    });
+});
+*/
